95fa49589cb83bd201de85e76f92c49ea5542f10
Colin454 <blackcolin138@gmail.com>
2025-06-03 00:24:44 +0700

    .

--- malefashion-master/backend/Server.js
+++ malefashion-master/backend/Server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -15,4 +15,3 @@ app.use(express.static(path.join(__dirname, '../')));
 app.listen(PORT, () => {
     console.log("Server is running at http://localhost:" + PORT);
 });
-//dsad
