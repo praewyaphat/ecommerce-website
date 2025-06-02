8de996e96fc60753352eff1e8628a372bbcc7dab
praewyaphat <ppraewpat2327@gmail.com>
2025-06-02 23:59:52 +0700

    run server

---
+++ malefashion-master/backend/Server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname));
+
+
+app.listen(PORT, () => {
+    console.log("Server is running at http://localhost:" + PORT);
+});
