ceca48cef8470f4a1c0d8a369a94e0ffd8087929
Colin454 <blackcolin138@gmail.com>
2025-06-03 12:06:55 +0700

    update database

--- malefashion-master/backend/init-db.js
+++ malefashion-master/backend/init-db.js
@@ -15,14 +15,24 @@ db.exec(`
   );
 `);
 
-//  เพิ่มตัวอย่างข้อมูล
+//  เพิ่มข้อมูล
 const insert = db.prepare('INSERT INTO products (name, price, category, image) VALUES (?, ?, ?, ?)');
 const sampleData = [
-  ['Jacket', 500, 'clothing', 'img/product/product-1.jpg'],
-  ['T-Shirt', 300, 'clothing', 'img/product/product-2.jpg'],
-  ['Sneakers', 1200, 'shoes', 'img/product/product-3.jpg']
+  ['Blown Jacket', 67.24, 'clothing', 'img/product/product-1.jpg'],
+  ['Sneaker Blue', 43.48, 'shoes', 'img/product/product-2.jpg'],
+  ['Shirt hood', 60.90, 'clothing', 'img/product/product-3.jpg'],
+  ['Jeans', 98.49, 'clothing', 'img/product/product-4.jpg'],
+  ['Backpack Pocket', 49.66, 'bags', 'img/product/product-5.jpg'],
+  ['Shirt Blue', 26.28, 'clothing', 'img/product/product-6.jpg'],
+  ['Black Shirt', 67.24, 'clothing', 'img/product/product-7.jpg'],
+  ['Perfume', 43.48, 'accessories', 'img/product/product-8.jpg'],
+  ['Backpack', 60.90, 'bags', 'img/product/product-9.jpg'],
+  ['Hoody', 98.49, 'clothing', 'img/product/product-10.jpg'],
+  ['Work Briefcase', 49.66, 'bags', 'img/product/product-11.jpg'],
+  ['Glasses', 26.28, 'accessories', 'img/product/product-12.jpg']
 ];
+
 
 sampleData.forEach(row => insert.run(...row));
 
-console.log('✅ สร้างฐานข้อมูลและเพิ่มข้อมูลสำเร็จ');
+console.log('สร้างฐานข้อมูลและเพิ่มข้อมูลสำเร็จ');
