ca39e069b3b990efc333561833aeecf075eea720
praewyaphat <ppraewpat2327@gmail.com>
2025-06-25 22:50:18 +0700

    register + login

--- malefashion-master/backend/Server.js
+++ malefashion-master/backend/Server.js
@@ -19,6 +19,8 @@ app.use('/api/products', require('./routes/products'));
 app.use('/api/search',   require('./routes/search'));
 app.use('/api/cart',     require('./routes/cart'));
 app.use('/api/admin/products', require('./routes/admin'));
+app.use('/api/register', require('./routes/register'));
+app.use('/api/login', require('./routes/login'));
 
 
 
